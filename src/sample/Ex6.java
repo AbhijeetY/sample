8e353062bdf1c7e8be7aa77623a7ca91ed581436
Sachin Nanaware <sachin@logtera.com>
2019-02-23 13:46:30 +0530

    Added test instant file

---
+++ src/sample/Ex6.java
@@ -0,0 +1,16 @@
+package sample;
+
+import java.time.Instant;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Ex6 {
+
+	public static void main(String[] args) {
+
+		Instant instant = Calendar.getInstance().toInstant();
+		System.out.println(instant);
+		System.out.println(instant.isBefore(new Date().toInstant()));
+	}
+
+}
